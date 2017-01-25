55e3911517a888852e54410ae43b8167e10cd11d
monf <monf@gmail.com>
2017-01-26 00:26:20 +0100

    [Feature] Update the neuron with the READ action
    + add the READ action
    + add READ related tests
    + add gitignore file

--- slack.py
+++ slack.py
@@ -1,7 +1,10 @@
-from slackclient import SlackClient
 import requests
+import json
 
-from kalliope.core.NeuronModule import NeuronModule, InvalidParameterException, MissingParameterException
+from slackclient import SlackClient
+
+from kalliope import Utils
+from kalliope.core.NeuronModule import NeuronModule, MissingParameterException
 
 Slack_Actions = (
     "POST",
@@ -16,14 +19,14 @@ class Slack(NeuronModule):
 
         # parameters
         self.action = kwargs.get('action', None)
-        self.slack_token = kwargs.get('slack_token', None)
-        self.channel = kwargs.get('slack_channel', None)
+        self.token = kwargs.get('slack_token', None)
+        self.channel = kwargs.get('channel', None)
         self.message = kwargs.get('message', None)
         self.nb_messages = int(kwargs.get('nb_messages', 10))  # Int
         
         # check parameters
         if self._is_parameters_ok():
-            sc = SlackClient(self.slack_token)
+            sc = SlackClient(self.token)
 
             if self.action == Slack_Actions[0]:  # POST
                 if self._is_post_parameters_ok():
@@ -43,17 +46,28 @@ class Slack(NeuronModule):
 
             if self.action == Slack_Actions[1]:  # READ
                 if self.is_read_parameters_ok():
-                    # TODO get the list of channel
-                    # user_list = sc.api_call("users.list")
-                    #
-                    # user_message_list = list()
-                    #
-                    # message = {
-                    #     "action": self.action,
-                    #     "messages": user_message_list,
-                    #     "channel": self.channel
-                    # }
-                    pass
+                    # get the list of channel
+                    user_list = sc.api_call("users.list")
+                    channel_list = sc.api_call("channels.list")
+
+                    # Find the channel ID of the given channel name
+                    channel_id = self._get_channel_id(channel_name=self.channel,
+                                                      channel_list=channel_list)
+
+                    # Get all messages of the channel
+                    messages_list = self._get_list_messages(token=self.token,
+                                                            channel_id=channel_id,
+                                                            nb_messages=self.nb_messages)
+
+                    # Associate user ID of messages to the user name
+                    user_messages = self._get_user_message_list(user_list=user_list,
+                                                                messages=messages_list)
+
+                    message = {
+                        "action": self.action,
+                        "messages": user_messages,
+                        "channel": self.channel
+                    }
 
             self.say(message)
 
@@ -64,7 +78,7 @@ class Slack(NeuronModule):
 
         .. raises:: MissingParameterException
         """
-        if self.slack_token is None:
+        if self.token is None:
             raise MissingParameterException("Slack needs a slack_token")
         if self.action is None:
             raise MissingParameterException("Slack needs an action parameter")
@@ -95,6 +109,88 @@ class Slack(NeuronModule):
             raise MissingParameterException("Slack needs a slack_channel")
 
         return True
+
+    @staticmethod
+    def _get_list_messages(token=None,
+                           channel_id=None,
+                           nb_messages=None):
+        """
+        Using Slack API to access messages from a given channel id.
+        :param token: the API token
+        :param channel_id: the channel id
+        :param nb_messages: the number of messages
+        :return: the message list of the last nb_messages
+        """
+        message_list = list()
+
+        data = {
+            "token": token,
+            "channel": channel_id
+        }
+
+        r = requests.post("https://slack.com/api/channels.history",
+                          data=data)
+
+        if r.status_code == 200:
+            json_data = json.loads(r.text)
+            if "messages" in json_data:
+                count = 0
+                for message in json_data["messages"]:
+                    if count < nb_messages:
+                        message_list.append(message)
+                        count += 1
+                    else: break
+
+        return message_list
+
+    @staticmethod
+    def _get_channel_id(channel_name=None,
+                        channel_list=None):
+        """
+        return the id from the channel list corresponding to the channel name.
+        :param channel_name: str of the name
+        :param channel_list: list of the channel
+        :return: the id from the channel list corresponding to the channel name.
+        """
+        id = None
+        if "channels" in channel_list:
+            for channel in channel_list["channels"]:
+                if "name" in channel:
+                    if channel_name == channel["name"]:
+                        if "id" in channel:
+                            id = channel["id"]
+        if id is None:
+            Utils.print_warning("The channel name has not been found !")
+        return id
+
+    @staticmethod
+    def _get_user_message_list(user_list=None,
+                               messages=None):
+        """
+        List of associate message to an user.
+        :param user_list: the full list of user
+        :param messages: the list of messages
+        :return: the list of dicts user:message
+        """
+        current_user_message_dict = dict()
+        user_message_list = list()
+
+        for message in messages:
+            if "username" in message:
+                current_user_message_dict[message["username"]] = message["text"]
+                user_message_list.append(current_user_message_dict)
+                current_user_message_dict = dict()
+                continue
+            elif "user" in message:
+                for user in user_list["members"]:
+                    if "id" in user:
+                        if user["id"] == message["user"]:
+                            current_user_message_dict[user["name"]] = message["text"]
+                            user_message_list.append(current_user_message_dict)
+                            current_user_message_dict = dict()
+                            continue
+
+        return user_message_list
 
 
 
--- tests/test_slack_neuron.py
+++ tests/test_slack_neuron.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 import mock
@@ -58,3 +59,92 @@ class TestSlack(unittest.TestCase):
                 "message": self.message
             }
             run_test(parameters)
+
+    def test_get_list_messages(self):
+        """
+        Testing the _get_list_messages method
+        """
+        token = "testtoken"
+        channel_id = "testchannelid"
+        nb_messages = 3
+
+        mocked_text = {
+            "messages": [
+                {"username": "Kalliope", "text": "hi"},
+                      {"name": "MONF42", "text": "hi K"},
+                      {"username": "Kalliope", "text": "how are you?"},
+                      {"name": "MONF42", "text": "fine"},
+                      {"username": "Kalliope", "text": "good"}
+            ]
+        }
+        # Encode
+        mocked_text = json.dumps(mocked_text, separators=(',', ':'))
+
+        expected_result = [
+            {"username": "Kalliope", "text": "hi"},
+            {"name": "MONF42", "text": "hi K"},
+            {"username": "Kalliope", "text": "how are you?"}
+        ]
+
+        with mock.patch("requests.post") as mock_requests_post:
+            mock_requests_post.return_value = mock.Mock(status_code=200,
+                                                        text=mocked_text)
+
+            self.assertEqual(Slack._get_list_messages(token=token,
+                                                      channel_id=channel_id,
+                                                      nb_messages=nb_messages),
+                             expected_result,
+                             "Fail to get the proper number of messages")
+
+    def test_get_channel_id(self):
+        """
+        Testing the _get_channel_id method
+        """
+        channel_name = "test"
+        channel_list = {
+            "channels": [
+                {"id": 'CK88', "name": "kalliope"},
+                {"id": 'OQY89', "name": "general"},
+                {"id": '8OLK0', "name": "random"},
+                {"id": 'MONF42', "name": "test"}
+            ]
+        }
+
+        expected_result = 'MONF42'
+
+        self.assertEqual(Slack._get_channel_id(channel_name=channel_name,
+                                               channel_list=channel_list),
+                         expected_result,
+                         "Fail to get the channel ID ")
+
+    def test_get_user_message_list(self):
+        """
+        Testing the _get_user_message_list method
+        """
+        user_list = {
+            "members":[
+                {"id": 'MONF42', "name": "monf"},
+                {"id": 'BOT66', "name": "bot"},
+                {"id": 'RDM90', "name": "myRandomUser"}
+            ]
+        }
+
+        messages = [
+            {"user": "MONF42", "text": "iam the test"},
+            {"user": "BOT66", "text": "iam the bot"},
+            {"user": "MONF42", "text": "hi bot"},
+            {"username": "Kalliope", "text": "Hi everybody"}
+        ]
+
+        expected_result = [
+            {"monf":"iam the test"},
+            {"bot": "iam the bot"},
+            {"monf": "hi bot"},
+            {"Kalliope": "Hi everybody"}
+        ]
+
+        self.assertEquals(Slack._get_user_message_list(user_list=user_list,
+                                                       messages=messages),
+                          expected_result,
+                          "Fail to deliver the user message")
+
